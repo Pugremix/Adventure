a69345262a0e6763b38944a9bdfcc562e546e10f
pugremix585@gmail.com <C0nn0rQu1nn>
2022-02-05 16:58:05 -0700

    Change

--- main.py
+++ main.py
@@ -9,19 +9,36 @@ pygame.display.set_caption("Adventure")
 # Fill background
 screen.fill((150, 150, 150))
 
+# Draw character
+x = 375
+y = 500
+width = 40
+height = 50
+vel = 5
+
 # Run until user asks to quit
 running = True
 while running:
+    pygame.time.delay(40)
 
     # Did user click the close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Draw
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    keys = pygame.key.get_pressed()
 
-    # Flip display
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    screen.fill((150, 150, 150))
+    pygame.draw.rect(screen, (255, 255, 255), (x, y, width, height))
     pygame.display.flip()
 
 # Quit
