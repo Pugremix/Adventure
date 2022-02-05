88b6f5dcf654bf2cc9f3887d69b72fd7d3b02b4c
pugremix585@gmail.com <C0nn0rQu1nn>
2022-02-05 16:13:33 -0700

    Change

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# Simple pygame program
-
 # Import and initialize the pygame library
 import pygame
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([900, 700])
 
 # Run until the user asks to quit
 running = True
