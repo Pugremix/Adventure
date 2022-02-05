f3be700e8761f25b7b751698946d7fb84f034e3e
pugremix585@gmail.com <C0nn0rQu1nn>
2022-02-05 16:24:13 -0700

    Change

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-# Import and initialize the pygame library
+# Import and initialize pygame library
 import pygame
 pygame.init()
 
 # Window
-screen = pygame.display.set_mode([900, 700])
+screen = pygame.display.set_mode([800, 960])
 pygame.display.set_caption("Adventure")
 
-# Run until the user asks to quit
+# Fill background
+screen.fill((150, 150, 150))
+
+# Run until user asks to quit
 running = True
 while running:
 
-    # Did the user click the close button?
+    # Did user click the close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # Fill background
-    screen.fill((150, 150, 150))
 
     # Draw
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
