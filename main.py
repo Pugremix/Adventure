9f463641316d58ef1c22768e321ff401f67ada49
pugremix585@gmail.com <C0nn0rQu1nn>
2022-02-06 17:01:36 -0700

    Change

--- main.py
+++ main.py
@@ -12,14 +12,16 @@ x = 375
 y = 500
 width = 20
 height = 25
+character = pygame.Rect(x, y, width, height)
 vel = 5
 # Walls
 vertical_width = 40
 vertical_height = 850
 horizontal_width = 700
 horizontal_height = 50
-left_wall = (0, 0, vertical_width, vertical_height)
-right_wall = (760, 0, vertical_width, vertical_height)
+left_wall = pygame.Rect(0, 0, vertical_width, vertical_height)
+right_wall = pygame.Rect(760, 0, vertical_width, vertical_height)
+room_1 = [left_wall, right_wall]
 
 # Run until user asks to quit
 running = True
@@ -42,10 +44,20 @@ while running:
     if keys[pygame.K_DOWN]:
         y += vel
 
+    # Collisions
+    if pygame.Rect.colliderect(left_wall, character) == True:
+        x += vel
+    if pygame.Rect.colliderect(right_wall, character) == True:
+        x -= vel
+
+    # Draw
     screen.fill((150, 150, 150))
+
     pygame.draw.rect(screen, color, left_wall)
     pygame.draw.rect(screen, color, right_wall)
-    pygame.draw.rect(screen, color, (x, y, width, height))
+
+    pygame.draw.rect(screen, color, character)
+    character = pygame.Rect(x, y, width, height)
     pygame.display.flip()
 
 # Quit
