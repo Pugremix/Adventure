def98717d6672d03e6648866e819ec171e17460e
pugremix585@gmail.com <C0nn0rQu1nn>
2022-04-20 13:31:41 -0600

    Change import sys

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import sys
 
 pygame.init()
 
-#Greg Pringle - needed to add this function to build .exe for Mr. Manning
-if getattr(sys, 'frozen', False):  #change directory to temp unpack folder if run from single compressed application
+# Greg Pringle - needed to add this function to build .exe for Mr. Manning
+if getattr(sys, 'frozen', False):  # Change directory to temp unpack folder if run from single compressed application
     os.chdir(sys._MEIPASS)
 
 
@@ -14,15 +14,25 @@ if getattr(sys, 'frozen', False):  #change directory to temp unpack folder if ru
 screen = pygame.display.set_mode([800, 750])
 pygame.display.set_caption("Adventure")
 
+# Errors
+HasError = False
+ErrorFont = pygame.font.Font(pygame.font.get_default_font(), 35)
+
 # Objects
 # All
 color = (255, 255, 255)
 vel = 5
 # Character (Retrieves information about Character from the game library)
 player = game_library.Character()
-# Load images
-key_yellow = game_library.Item('yellow_key.png', 1, 120, 205, 120, 205)
-yellow_castle = game_library.Castle('yellow_castle.png', 'Gate.png', 1)
+# Load images (Loads game assets, but if not found gives an error message)
+try:
+    key_yellow = game_library.Item('yellow_key.png', 1, 120, 205, 120, 205)
+    yellow_castle = game_library.Castle('yellow_castle.png', 'Gate.png', 1)
+except FileNotFoundError:
+    screen.fill((0, 0, 0))
+    text_surface = ErrorFont.render('Game assets not found', False, color)
+    screen.blit(text_surface, (10,10))
+    HasError = True
 # Walls
 # General
 vertical_width = 40
@@ -64,125 +74,127 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
+    if (HasError != True):
 
-    # Update Character position
-    player.update_character()
-    # Yellow Key
-    draw_yellow = pygame.Rect(key_yellow.item_x, key_yellow.item_y, 40, 15)
-    key_yellow.update_items()
+        keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        player.x -= vel
-        if player.item_held != None:
-            player.item_held.item_x -= vel
-    if keys[pygame.K_RIGHT]:
-        player.x += vel
-        if player.item_held != None:
-            player.item_held.item_x += vel
-    if keys[pygame.K_UP]:
-        player.y -= vel
-        if player.item_held != None:
-            player.item_held.item_y -= vel
-    if keys[pygame.K_DOWN]:
-        player.y += vel
-        if player.item_held != None:
-            player.item_held.item_y += vel
-    if keys[pygame.K_SPACE]:
-        player.item_held = None
+        # Update Character position
+        player.update_character()
+        # Yellow Key
+        draw_yellow = pygame.Rect(key_yellow.item_x, key_yellow.item_y, 40, 15)
+        key_yellow.update_items()
 
-    # Collisions
-    player.hitdetect(rooms)
+        if keys[pygame.K_LEFT]:
+            player.x -= vel
+            if player.item_held != None:
+                player.item_held.item_x -= vel
+        if keys[pygame.K_RIGHT]:
+            player.x += vel
+            if player.item_held != None:
+                player.item_held.item_x += vel
+        if keys[pygame.K_UP]:
+            player.y -= vel
+            if player.item_held != None:
+                player.item_held.item_y -= vel
+        if keys[pygame.K_DOWN]:
+            player.y += vel
+            if player.item_held != None:
+                player.item_held.item_y += vel
+        if keys[pygame.K_SPACE]:
+            player.item_held = None
 
-    # New room
-    # Yellow Castle
-    if (player.y < 300) and (345 < player.x < 425) and (player.room_number == 1):
-        player.y = 705
-        player.room_number = 0
-        if player.item_held != None:
-            player.item_held.item_y += 410
-            player.item_held.item_room = 0
-    if (player.y > 725) and (player.room_number == 0):
-        player.y = 320
-        player.x = 375
-        player.room_number = 1
-        if player.item_held != None:
-            player.item_held.item_y -= 410
-            player.item_held.item_x += player.x - player.old_x
-            player.item_held.item_room = 1
+        # Collisions
+        player.hitdetect(rooms)
 
-    if (player.y > 725) and (player.room_number == 1):
-        player.y = 20
-        player.room_number = 2
-        if player.item_held != None:
-            player.item_held.item_y -= 710
-            player.item_held.item_room = 2
-    if (player.y < 0) and (player.room_number == 2):
-        player.y = 705
-        player.room_number = 1
-        if player.item_held != None:
-            player.item_held.item_y += 710
-            player.item_held.item_room = 1
-    if (player.x > 780) and (player.room_number == 2):
-        player.x = 20
-        player.room_number = 3
-        if player.item_held != None:
-            player.item_held.item_x -= 765
-            player.item_held.item_room = 3
-    if (player.x < 0) and (player.room_number == 3):
-        player.x = 760
-        player.room_number = 2
-        if player.item_held != None:
-            player.item_held.item_x += 765
-            player.item_held.item_room = 2
-    if (player.x < 0) and (player.room_number == 2):
-        player.x = 760
-        player.room_number = 4
-        if player.item_held != None:
-            player.item_held.item_x += 765
-            player.item_held.item_room = 4
-    if (player.x > 780) and (player.room_number == 4):
-        player.x = 20
-        player.room_number = 2
-        if player.item_held != None:
-            player.item_held.item_x -= 765
-            player.item_held.item_room = 2
+        # New room
+        # Yellow Castle
+        if (player.y < 300) and (345 < player.x < 425) and (player.room_number == 1):
+            player.y = 705
+            player.room_number = 0
+            if player.item_held != None:
+                player.item_held.item_y += 410
+                player.item_held.item_room = 0
+        if (player.y > 725) and (player.room_number == 0):
+            player.y = 320
+            player.x = 375
+            player.room_number = 1
+            if player.item_held != None:
+                player.item_held.item_y -= 410
+                player.item_held.item_x += player.x - player.old_x
+                player.item_held.item_room = 1
 
-    # Room properties
-    if (player.room_number == 0):
-        color = (200, 200, 0)
-    if (player.room_number == 1):
-        color = (200, 200, 0)
-    if (player.room_number == 2):
-        color = (0, 210, 70)
-    if (player.room_number == 3):
-        color = (130, 150, 50)
-    if (player.room_number == 4):
-        color = (100, 175, 50)
+        if (player.y > 725) and (player.room_number == 1):
+            player.y = 20
+            player.room_number = 2
+            if player.item_held != None:
+                player.item_held.item_y -= 710
+                player.item_held.item_room = 2
+        if (player.y < 0) and (player.room_number == 2):
+            player.y = 705
+            player.room_number = 1
+            if player.item_held != None:
+                player.item_held.item_y += 710
+                player.item_held.item_room = 1
+        if (player.x > 780) and (player.room_number == 2):
+            player.x = 20
+            player.room_number = 3
+            if player.item_held != None:
+                player.item_held.item_x -= 765
+                player.item_held.item_room = 3
+        if (player.x < 0) and (player.room_number == 3):
+            player.x = 760
+            player.room_number = 2
+            if player.item_held != None:
+                player.item_held.item_x += 765
+                player.item_held.item_room = 2
+        if (player.x < 0) and (player.room_number == 2):
+            player.x = 760
+            player.room_number = 4
+            if player.item_held != None:
+                player.item_held.item_x += 765
+                player.item_held.item_room = 4
+        if (player.x > 780) and (player.room_number == 4):
+            player.x = 20
+            player.room_number = 2
+            if player.item_held != None:
+                player.item_held.item_x -= 765
+                player.item_held.item_room = 2
 
-    # Draw
-    # Background
-    screen.fill((150, 150, 150))
-    # Draw room loop
-    for walls in rooms[player.room_number]:
-        if (walls != gate):
-            pygame.draw.rect(screen, color, walls)
+        # Room properties
+        if (player.room_number == 0):
+            color = (200, 200, 0)
+        if (player.room_number == 1):
+            color = (200, 200, 0)
+        if (player.room_number == 2):
+            color = (0, 210, 70)
+        if (player.room_number == 3):
+            color = (130, 150, 50)
+        if (player.room_number == 4):
+            color = (100, 175, 50)
 
-    # Draw Character
-    player.draw_character(screen, color)
+        # Draw
+        # Background
+        screen.fill((150, 150, 150))
+        # Draw room loop
+        for walls in rooms[player.room_number]:
+            if (walls != gate):
+                pygame.draw.rect(screen, color, walls)
 
-    # Foreground
-    # Draw Castles
-    if (player.room_number == 1):
-        yellow_castle.draw_castle(screen)
+        # Draw Character
+        player.draw_character(screen, color)
 
-    # Draw Items
-    if (player.room_number == key_yellow.item_room):
-        key_yellow.draw_items(screen)
-    if (player.room_number == key_yellow.item_room) and pygame.Rect.colliderect(draw_yellow, player.character):
-        player.item_held = key_yellow
-        key_yellow.item_x += player.x - player.old_x
-        key_yellow.item_y += player.y - player.old_y
+        # Foreground
+        # Draw Castles
+        if (player.room_number == 1):
+            yellow_castle.draw_castle(screen)
+
+        # Draw Items
+        if (player.room_number == key_yellow.item_room):
+            key_yellow.draw_items(screen)
+        if (player.room_number == key_yellow.item_room) and pygame.Rect.colliderect(draw_yellow, player.character):
+            player.item_held = key_yellow
+            key_yellow.item_x += player.x - player.old_x
+            key_yellow.item_y += player.y - player.old_y
 
     pygame.display.flip()
 
